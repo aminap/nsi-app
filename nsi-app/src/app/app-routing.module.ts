bb1dff0a29d8ba003431f5eaa0f88ea140a13a56
NerminImamovic <nermin_im@hotmail.com>
2018-10-28 18:46:40 +0100

    device-type-dashboard

--- nsi-app/src/app/app-routing.module.ts
+++ nsi-app/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AddDeviceModalComponent } from 'src/app/devices-dashboard/add-device-modal/add-device-modal.component';
+import { DeviceTypesDashboardComponent } from './device-types-dashboard/device-types-dashboard.component';
+import { DevicesDashboardComponent } from './devices-dashboard/devices-dashboard.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  { path: 'add-device', component: AddDeviceModalComponent }
+  { path: 'dashboard', component: DevicesDashboardComponent},
+  { path: 'add-device', component: AddDeviceModalComponent },
+  { path: 'device-type-dashboard', component: DeviceTypesDashboardComponent },
 ];
 
 @NgModule({
